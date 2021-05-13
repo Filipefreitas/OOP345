cf4f334730de2958e2e4abb7f3d070b55a0411b9
Filipefreitas <48395687+Filipefreitas@users.noreply.github.com>
2021-05-13 15:13:33 -0300

    Add files via upload

---
+++ Project/ms3/Utilities.h
@@ -0,0 +1,27 @@
+// Name: Filipe da Cunha de Freitas
+// Seneca Student ID: 155737190
+// Seneca email: fda-cunha-de-freitas@gmyseneca.ca
+// Date of completion: 2021-04-11
+// Milestone 3
+//
+// I confirm that I am the only author of this file and the content was created entirely by me.
+
+#ifndef UTILITIES_H
+#define UTILITIES_H
+
+namespace sdds
+{
+	class Utilities 
+	{
+		size_t m_widthField = 1;
+		static char m_delimiter;
+	public:
+		void setFieldWidth(size_t newWidth);
+		size_t getFieldWidth() const;
+		std::string extractToken(const std::string& str, size_t& next_pos, bool& more);
+		static void setDelimiter(char newDelimiter);
+		static char getDelimiter();
+	};
+}
+
+#endif 
